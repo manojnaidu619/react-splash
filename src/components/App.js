e1487cd56922dd3670eaac089b481511a0de0433
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-18 13:30:37 +0530

    Added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
     callMeOnSubmit(term){
         console.log(term)
     }
+
     render(){
         return (
             <div className="ui container" style={{marginTop: "20px"}}>
